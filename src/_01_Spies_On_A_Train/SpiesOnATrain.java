27cafbfbd46e666ed2d282fe6b57ea7bb238db06
league <maddoxseul@sbsd.net>
2025-12-28 11:32:50 -0800

    Gyatatat

--- src/_01_Spies_On_A_Train/SpiesOnATrain.java
+++ src/_01_Spies_On_A_Train/SpiesOnATrain.java
@@ -1,5 +1,6 @@
 package _01_Spies_On_A_Train;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import _00_Intro_to_Linked_Lists.LinkedList;
@@ -22,7 +23,58 @@ public class SpiesOnATrain {
      * statements.
      */
     String findIntel(LinkedList<TrainCar> train, String[] clues) {
-
+    
+//    for(int i =0 ; i < clues.length; i++) {
+//    	System.out.println(clues[i]);
+//    }
+//    
+//    Node<TrainCar> current = train.getHead();
+//    
+//    do {
+//    	System.out.println(current.getValue().questionPassenger());
+//    	
+//    	current = current.getNext();
+//    	
+//    }while(current.getNext() != null);
+    	ArrayList<CharSequence> names = new ArrayList<CharSequence>();
+    	ArrayList<String> evidence = new ArrayList<String>();
+    
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    
+    	while( train.getHead().getNext() != null) {
+    		names.add(train.getHead().getValue().toString().subSequence(0, train.getHead().getValue().toString().length()));
+    		System.out.println( train.getHead().getValue().questionPassenger() );
+    		CharSequence rest = train.getHead().getValue().questionPassenger().subSequence(37, train.getHead().getValue().questionPassenger().length());
+    		evidence.add(rest.toString());
+    		train.setHead(train.getHead().getNext());
+    		
+    	}
+    	
+    	for( int n = 0; n < evidence.size(); n++) {
+    		for( int i = 0; i < names.size(); i++) {
+    	  //  	System.out.println(	names.get(i) );
+    	    	if( evidence.get(n).contains(names.get(i))) {
+    	    		System.out.println(names.get(i));
+    	    	}
+    		}
+    		
+    	}
+    
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+ 
+    	
         return "";
 
     }
